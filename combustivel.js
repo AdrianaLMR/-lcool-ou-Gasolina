fc600adcb5145c63366c7b67dbacad53835d16df
Adriana LMR <trevenlynk038@gmail.com>
2022-05-24 19:10:31 -0300

    Efetuando o primeiro commit de álcool ou gasolina

---
+++ combustivel.js
@@ -0,0 +1,35 @@
+
+
+
+
+console.log('-------------------------------');
+console.log('Insira os valores e descubra qual combustivel é mais vantajoso para você!');
+console.log('-------------------------------');
+
+do {
+    var gasolina = prompt('Digite o valor da gasolina: ');
+    var álcool = prompt('Digite o valor do álcool: ');
+    var repetir = true;
+    var multiplicador = (parseInt(álcool) / parseInt(gasolina));
+    console.log('O resultado da operação é: ' + multiplicador);
+
+    if (multiplicador > 0.7) {
+        console.log(' Vantagem da gasolina');
+    } else if (multiplicador < 0.7) {
+        console.log(' Vantagem do Álcool');
+    } else if (multiplicador == 0.7) {
+        console.log(' Ambos combustiveis são iguais, não há vantagem');
+    } else {
+        console.log('Para repetir a operação basta digitar 1');
+    }
+    resposta = prompt('Gostaria de efetuar uma nova operação(s/n): ');
+    if (resposta == 'n') {
+        repetir = false;
+    }
+} while (repetir);
+
+console.log('-------------------------------');
+console.log('Muito obrigado');
+console.log('Programa finalizado');
+console.log('-------------------------------');
+
